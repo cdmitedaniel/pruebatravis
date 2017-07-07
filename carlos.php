1791b9a9433b8a43198571294cee19014bd796eb
cdmitedaniel <cdmitedaniel@gmail.com>
2017-07-07 13:57:56 -0500

    Update carlos.php

--- carlos.php
+++ carlos.php
@@ -1,11 +1,9 @@
 <?php
 
-class HelloWorldTest extends WP_UnitTestCase {}
- class HelloWorldTest extends WP_UnitTestCase {
-  function test_hello_world() {
-    // Test hello_world()
-  }
+class HelloWorldTest extends WP_UnitTestCase {
+
 function test_hello_world() {
   $this->assertEquals("Hello World!" == hello_world());
+}
 }
 
