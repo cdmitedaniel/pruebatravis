563c2c88ba29681fe45c6bbe438792fe4d79cee5
cdmitedaniel <cdmitedaniel@gmail.com>
2017-07-07 14:08:18 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
-/**
- * @package Hello_World
- * @version 0.1
- *
- * Plugin Name: Hello World!
- * Plugin URI: https://gist.github.com/rnagle/d561bd58504a644e9657
- * Description: Just a simple WordPress plugin that prints "Hello world!" in the top corner of the WordPress dashboard.
- * Author: Ryan Nagle
- * Version: 0.1
- **/
-
-function hello_world() {
-  return "Hello world!";
+require 'calculadora.php';
+class CalculatorTests extends PHPUnit_Framework_TestCase
+{
+private $calculator;
+protected function setUp()
+{
+$this->calculator= new Calculator();
+}
+public function testAdd()
+{
+$result=$this->calculator->add(1,2);
+$this->assertEquals(3,$result);
+}
 }
 
